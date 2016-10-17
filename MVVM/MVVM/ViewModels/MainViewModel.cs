94e0ee1c998e4aa21bc8ae1e48d6d1df172d397f
Sergio <Sergio@SERGIO>
2016-10-17 14:50:16 -0500

    Main Page, Menu Page, Master Page and Icons added

---
+++ MVVM/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MVVM.ViewModels
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<MenuItemViewModel> Menu { get; set; }
+
+        public MainViewModel()
+        {
+            LoadMenu();
+        }
+
+        private void LoadMenu()
+        {
+            Menu = new ObservableCollection<MenuItemViewModel>();
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_orders.png",
+                PageName = "NewOrderPage",
+                Title = "Pedidos",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_customers.png",
+                PageName = "ClientsPage",
+                Title = "Clientes",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_alarm.png",
+                PageName = "AlarmsPage",
+                Title = "Alarmas",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_settings.png",
+                PageName = "SettingsPage",
+                Title = "Ajustes",
+            });
+        }
+    }
+}
